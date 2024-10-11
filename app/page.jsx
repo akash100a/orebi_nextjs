3d188beee0b0aa103ec974d91ff7ffe8da103efc
akash100a <arakash968786@gmail.com>
2024-10-11 17:48:24 +0600

    up

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,15 @@
 import React from 'react'
 
 const Home = () => {
+  let ar = ["rimjim","Akash","Aeyat"]
+  let newar= ar.map((val)=>{
+    return val.toLowerCase()
+  })
+  console.log(ar);
+  console.log(newar);
+  console.log(newar.includes("rimjim"));
+  console.log(newar.indexOf("aeyat",-1));
+  
   return (
     <div>Home Page</div>
   )
